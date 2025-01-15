8f29aa62bc96cae1eac1f17c1b862851b451a6a0
alek <wait82985@gmail.com>
2025-01-15 15:59:00 +0500

    better code

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,28 +1,28 @@
-public class Solution {
+class Solution {
     public int minimizeXor(int num1, int num2) {
-        int count = Integer.bitCount(num2);
-        String bin = Integer.toBinaryString(num1);
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < bin.length(); i++){
-            if(bin.charAt(i) == '1' && count > 0){
-                sb.append('1');
-                count--;
-            }
-            else sb.append('0');
-        }
-        if(count != 0){
-            int size = sb.length();
-            for(int i = size - 1; i >= 0; i--){
-                if(sb.charAt(i) == '0' && count > 0){
-                    sb.setCharAt(i, '1');
-                    count--;
+        int n2 = Integer.bitCount(num2);
+
+        int res = 0;
+        for(int i = 31; i >=0; i--) {
+            if((num1 & (1 << i)) != 0){
+                res |= 1 << i;
+                n2--;
+                if(n2 == 0) {
+                    return res;
                 }
             }
         }
-        while(count != 0){
-            sb.insert(0, '1');
-            count--;
+        for(int i = 0; i < 32; i++) {
+            if((num1 & (1 << i)) == 0){
+                res |= 1 << i;
+                n2--;
+                if(n2 == 0) {
+                    return res;
+                }
+
+            }
         }
-        return Integer.parseInt(sb.toString(), 2);
+
+        return res;
     }
 }
